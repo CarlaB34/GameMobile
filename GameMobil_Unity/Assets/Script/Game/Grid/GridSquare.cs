440b6b030ff18112940004c4c8a4c3690cc9c168
CarlaB34 <carlita.b34@gmail.com>
2022-01-12 14:44:47 +0100

    debut de la grid

--- GameMobil_Unity/Assets/Script/Game/Grid/GridSquare.cs
+++ GameMobil_Unity/Assets/Script/Game/Grid/GridSquare.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridSquare : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Image m_NormalImage;
+
+    [SerializeField]
+    private List<Sprite> m_NormalSpriteImages;
+
+
+    public void SetImage(bool p_SetFirstImage)
     {
-        
+        //15.37 de la video 3 (pas tous compris)
+        //comme un if avec hubert
+        //savoir si l'image qu'on a est egale a l'index, donner par le paramettre
+        //fonction appeler dans grid
+        m_NormalImage.GetComponent<Image>().sprite = p_SetFirstImage ? m_NormalSpriteImages[1] : m_NormalSpriteImages[0];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
